01225a572a9670ad302e20726a4f1e4d439faec5
PedroUchoa <joaopedrouchoacampos@gmail.com>
2024-08-28 16:54:24 -0300

    Add: Service de Mensagens

--- notificacao/src/main/java/com/pedro/notificacao/listener/ProposalNotIntergratedListener.java
+++ notificacao/src/main/java/com/pedro/notificacao/listener/ProposalNotIntergratedListener.java
@@ -1,12 +1,22 @@
 package com.pedro.notificacao.listener;
 
+import com.pedro.notificacao.constant.ConstantMessage;
+import com.pedro.notificacao.domain.Proposal;
+import com.pedro.notificacao.service.NotificationSnsService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProposalNotIntergratedListener {
 
-    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-    public void proposalNotIntegrated(){
+    @Autowired
+    private NotificationSnsService notificationSnsService;
 
+    @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+    public void proposalNotIntegrated(Proposal proposal){
+        String message =String.format(ConstantMessage.PROPOSTA_EM_ANALISE,proposal.getUsers().getName());
+        notificationSnsService.notification(message);
     }
 
 
---
+++ notificacao/src/main/java/com/pedro/notificacao/service/NotificationSnsService.java
@@ -0,0 +1,13 @@
+package com.pedro.notificacao.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationSnsService {
+
+
+    public void notification(String message){
+
+    }
+
+}
